c6b3a0a0282902cccf964bfd966c2877d9c181b6
babbdi <167539288+babbdi@users.noreply.github.com>
2025-03-31 23:43:56 -0300

    firstPush

---
+++ characters/player/scene/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody3D
+
+@export var head : Node3D
+@export var camera : Camera3D
+
+func _ready():
+	pass
+
+func _physics_process(delta: float) -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
