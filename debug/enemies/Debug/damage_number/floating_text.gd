866d425dcd7232d1b9516694261b96f1a648e05f
babbdi <167539288+babbdi@users.noreply.github.com>
2025-04-09 00:35:49 -0300

    amorinha

--- debug/enemies/Debug/damage_number/floating_text.gd
+++ debug/enemies/Debug/damage_number/floating_text.gd
@@ -5,5 +5,6 @@ static func spawn_floating_text(global_pos : Vector3, text : String, parent : No
 	var floating_text : Node3D = preload("res://debug/enemies/Debug/damage_number/floating_text.tscn").instantiate()
 	floating_text.mesh.text = text
 	parent.add_child(floating_text)
+	#var tween = Tween.new()
 	
 	print("oi")
